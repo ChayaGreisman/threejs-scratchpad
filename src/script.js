0980b754979a3800f27c41ba418a2f2f91f69adf
Chaya Greisman <chayamgreisman@gmail.com>
2024-09-15 20:14:51 -0400

    clearcoat, sheen

--- src/script.js
+++ src/script.js
@@ -122,12 +122,21 @@ material.normalMap = doorNormalTexture
 material.transparent = true
 material.alphaMap = doorAlphaTexture
 
-// Clearcoat
-material.clearcoat = 1
-material.clearcoatRoughness = 0
+// // Clearcoat
+// material.clearcoat = 1
+// material.clearcoatRoughness = 0
 
-gui.add(material, 'clearcoat').min(0).max(1).step(0.0001)
-gui.add(material, 'clearcoatRoughness').min(0).max(1).step(0.0001)
+// gui.add(material, 'clearcoat').min(0).max(1).step(0.0001)
+// gui.add(material, 'clearcoatRoughness').min(0).max(1).step(0.0001)
+
+// Sheen
+material.sheen = 1
+material.sheenRoughness = 0.25
+material.sheenColor.set(1, 1, 1)
+
+gui.add(material, 'sheen').min(0).max(1).step(0.0001)
+gui.add(material, 'sheenRoughness').min(0).max(1).step(0.0001)
+gui.addColor(material, 'sheenColor')
 
 
 
